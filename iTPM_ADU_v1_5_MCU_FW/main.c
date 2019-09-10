51c9d9d606971e2601914426061625f44475b19e
Luca Schettini <luca.schettini@sanitaseg.it>
2019-09-10 18:01:00 +0200

    SPI Clock 12MHz ok

--- iTPM_ADU_v1_5_MCU_FW/main.c
+++ iTPM_ADU_v1_5_MCU_FW/main.c
@@ -4,6 +4,9 @@
 
 int main(void)
 {
+	// Cheap delay startup ~1000 ms total
+	for (int i = 0; i < 0xffff; i++) asm("nop");
+	
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 	
@@ -17,7 +20,7 @@ int main(void)
 		gpio_toggle_pin_level(USR_LED0);
 		uint32_t vers;
 		XO3_Read(0x30000010, &vers);
-		XO3_WriteByte(0x30000010, 0x0123467);
+		XO3_WriteByte(0x30000010, 0x01234567);
 		XO3_Read(0x30000010, &vers);
 		XO3_WriteByte(0x30000010, 0x76543210);
 		XO3_Read(0x30000010, &vers);
