2d749366d5ef58a5e5d5e802499e8d8f994d977d
Luca Schettini <luca.schettini@sanitaseg.it>
2019-12-06 10:32:39 +0100

    mid2

---
+++ iTPM_ADU_v1_5_MCU_FW/ExtFlash.c
@@ -0,0 +1,27 @@
+/*
+ * ExtFlash.c
+ *
+ * Created: 05/12/2019 14:23:53
+ *  Author: luca
+ */ 
+
+#include <SpiRouter.h>
+#include <atmel_start.h>
+#include "regfile.h"
+
+#define FLASH0_SLID 1
+#define FLASH1_SLID 2
+
+void ExtFlash_SRAMErase(uint8_t fpgaid){
+	if (fpgaid == 0x1){
+		XO3_WriteByte(itpm_cpld_smap_xil_0, 0);
+		
+	}
+	if (fpgaid == 0x2){
+		XO3_WriteByte(itpm_cpld_smap_xil_1, 0);
+	}
+}
+
+void ExtFlash_FPGA_Prog(uint8_t fpgaid, uint8_t flashid){
+	
+}
---
+++ iTPM_ADU_v1_5_MCU_FW/ExtFlash.h
@@ -0,0 +1,16 @@
+/*
+ * ExtFlash.h
+ *
+ * Created: 05/12/2019 14:24:11
+ *  Author: luca
+ */ 
+
+
+#ifndef EXTFLASH_H_
+#define EXTFLASH_H_
+
+
+
+
+
+#endif /* EXTFLASH_H_ */
